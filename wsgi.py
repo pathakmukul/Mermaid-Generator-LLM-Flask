a15c82884049dfe5842ac86ab20efb2e4a66f8c6
Mukul Pathak <iammap26@gmail.com>
2024-10-05 17:42:48 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,4 @@
-import importlib.util
-import os
-import sys
+from mermaid import app
 
-# Add the path of your project to sys.path
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Load the module from mermaid.py using importlib
-spec = importlib.util.spec_from_file_location("mermaid", "/home/aneksbjl/flow1/mermaid.py")
-mermaid = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(mermaid)
-
-# Set the application variable for WSGI
-application = mermaid.app
+# WSGI callable for Vercel
+application = app
